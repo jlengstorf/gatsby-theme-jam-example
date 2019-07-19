dbf931f768807ac246fc0d7658b7aaac6b595b9a
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-19 23:54:39 +0200

    Add mdx

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,9 +1,14 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  plugins: ["gatsby-theme-waves"],
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    "gatsby-theme-waves",
+  ],
 }
---
+++ demo/src/components/layout.js
@@ -0,0 +1,12 @@
+import { Layout, Main, Container } from "theme-ui"
+import React from "react"
+
+export default function({ children }) {
+  return (
+    <Layout>
+      <Main>
+        <Container>{children}</Container>
+      </Main>
+    </Layout>
+  )
+}
