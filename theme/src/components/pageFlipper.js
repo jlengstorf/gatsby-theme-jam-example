b3f2f17e4dc7306dc498ef0f886ee36ec1e6b2cd
Ola Vea <ola@olavea.com>
2019-07-27 13:19:56 +0200

    PageFlipper as functional component #32 (#38)
    * Made PageFlipper a functional component, bulletpoint 1 in issue #32
    * Added a listener for keydown inside useEffects and console.log key is down on every keydown event. #32
    * Move on to console.log 'left arrow' when left arrow, and right arrow when right arrow. #32
    * Navigate to the previous page when the key is the left arrow. And Navigate to the next page when the key is the right arrow.
    * refactored
    * removing the listener #32

--- theme/src/components/pageFlipper.js
+++ theme/src/components/pageFlipper.js
@@ -1,61 +1,53 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import { Link, navigate } from "gatsby"
 
-class PageFlipper extends React.Component {
-  handleKeyStroke = event => {
-    const { nextPage, previousPage } = this.props
-
+const PageFlipper = ({ nextPage, previousPage }) => {
+  const tingeling = event => {
     if (event.keyCode === 37 && previousPage) {
       navigate(previousPage.path)
-    }
-
-    if (event.keyCode === 39 && nextPage) {
+    } else if (event.keyCode === 39 && nextPage) {
       navigate(nextPage.path)
     }
   }
+  useEffect(() => {
+    // Update the document title using the browser API
+    window.addEventListener("keydown", tingeling)
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyStroke)
-  }
+    return () => {
+      window.removeEventListener("keydown", tingeling)
+    }
+  })
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyStroke)
-  }
+  return (
+    <ul
+      style={{
+        background: `pink`,
+        display: `flex`,
+        flexWrap: `wrap`,
+        justifyContent: `space-between`,
+        listStyle: `none`,
 
-  render() {
-    const { nextPage, previousPage } = this.props
-
-    return (
-      <ul
-        style={{
-          background: `pink`,
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-
-          margin: 0,
-          marginBottom: 0,
-        }}
-      >
-        <li>
-          {previousPage && (
-            <Link to={`/${previousPage.path}`} rel="prev">
-              ← {previousPage.title}
-            </Link>
-          )}
-        </li>
-        <li>
-          {nextPage && (
-            <Link to={`/${nextPage.path}`} rel="next">
-              {nextPage.title} →
-            </Link>
-          )}
-        </li>
-      </ul>
-    )
-  }
+        margin: 0,
+        marginBottom: 0,
+      }}
+    >
+      <li>
+        {previousPage && (
+          <Link to={`/${previousPage.path}`} rel="prev">
+            ← {previousPage.title}
+          </Link>
+        )}
+      </li>
+      <li>
+        {nextPage && (
+          <Link to={`/${nextPage.path}`} rel="next">
+            {nextPage.title} →
+          </Link>
+        )}
+      </li>
+    </ul>
+  )
 }
 
 export default PageFlipper
