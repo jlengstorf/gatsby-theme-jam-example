5882f2d3ce3a4398a148618d6595d55d32bb8675
Matthew Weeks <mweeks@tribalscale.com>
2019-07-29 20:49:00 -0400

    fix(config): Fix package names and display data on index page

--- theme/src/templates/page.js
+++ theme/src/templates/page.js
@@ -1,12 +1,32 @@
 import React from "react"
 import { Styled } from "theme-ui"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
-const PageTemplate = ({ pageContext }) => (
-  <Layout>
-    <Styled.h1>{pageContext.heading}</Styled.h1>
-    <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
-  </Layout>
-)
+const PageTemplate = ({ pageContext }) => {
+  const data = useStaticQuery(graphql`
+    query InstaQuery {
+      allInstaNode {
+        edges {
+          node {
+            caption
+            likes
+            original
+            preview
+            type
+            username
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Styled.h1>{pageContext.heading}</Styled.h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </Layout>
+  )
+}
 
 export default PageTemplate
