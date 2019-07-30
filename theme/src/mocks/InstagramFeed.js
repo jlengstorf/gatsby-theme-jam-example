f6727ca3af8bfe971ba774a49bb5fba48e1b022b
Leire Polo Martin <lmartin@tribalscale.com>
2019-07-29 21:21:31 -0400

    chore(mocks): Add mocks folder and InstagramFeed mock.

---
+++ theme/src/mocks/InstagramFeed.js
@@ -0,0 +1,519 @@
+const InstagramFeed = [
+  {
+    node: {
+      id: "BM2P0tGjCb7",
+      likes: 359,
+      comments: 9,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1479249014,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 750,
+        width: 750,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BNC-Cl2DZjX",
+      likes: 419,
+      comments: 14,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1479675898,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1080,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BbRr3iGAhTW",
+      likes: 1819,
+      comments: 91,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1510234458,
+      caption: "insp.re",
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1080,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BpP4G28DsZd",
+      likes: 835,
+      comments: 85,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1540238537,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1080,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BpP3bLGDlTU",
+      likes: 1657,
+      comments: 33,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1540238179,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 750,
+        width: 750,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BQILFemj5WY",
+      likes: 5095,
+      comments: 116,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1486292961,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1078,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BOJznntDTFI",
+      likes: 217,
+      comments: 11,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1482052799,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1080,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BQBmo-qDffc",
+      likes: 205,
+      comments: 6,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1486072526,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1078,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BPqJ5p7jWOn",
+      likes: 314,
+      comments: 18,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1485285706,
+      caption: "When in doubt",
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1080,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BQDrAHgDMuJ",
+      likes: 319,
+      comments: 17,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1486141922,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1078,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BQF_9VxDSw-",
+      likes: 320,
+      comments: 25,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1486220018,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1078,
+        width: 1080,
+      },
+    },
+  },
+  {
+    node: {
+      id: "BQGs3hdjML9",
+      likes: 577,
+      comments: 78,
+      mediaType: "GraphImage",
+      preview: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      original: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+      timestamp: 1486243563,
+      caption: null,
+      thumbnails: [
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 150,
+          config_height: 150,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 240,
+          config_height: 240,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 320,
+          config_height: 320,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 480,
+          config_height: 480,
+        },
+        {
+          src: "https://www.instagram.com/p/BI0BQkfh0ed/media/?size=t",
+          config_width: 640,
+          config_height: 640,
+        },
+      ],
+      dimensions: {
+        height: 1078,
+        width: 1080,
+      },
+    },
+  },
+]
+export default InstagramFeed
