4acf018876e49ba2fae820448fa1ceac5c721ddc
Matthew Weeks <mweeks@tribalscale.com>
2019-07-29 20:31:37 -0400

    feat: Update theme name and add instagram and twitter themes

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,6 +1,13 @@
-module.exports = {
+module.exports = (options) => ({
   siteMetadata: {
     title: "Gatsby Theme Jam Example Submission",
   },
-  plugins: ["gatsby-plugin-theme-ui"],
-}
+  plugins: ["gatsby-plugin-theme-ui", 
+    {
+      resolve: "gatsby-source-instagram",
+      options: {
+        username: 'weeksling'
+      }
+    }
+  ],
+})
