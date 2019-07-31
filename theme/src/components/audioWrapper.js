be01c143cf24e35931160005afaa3beaf5171f43
Tom Erik Støwer <tomerik@tomerikstower.com>
2019-07-31 07:19:32 +0200

    Hide audio controls when there is no audio file (#46)

--- theme/src/components/audioWrapper.js
+++ theme/src/components/audioWrapper.js
@@ -13,37 +13,40 @@ const AudioWrapper = ({ children }) => (
     `}
     render={data => (
       <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-
+        {data.file ? (
           <div
             style={{
-              width: "100vw",
-              position: "fixed",
-              bottom: 0,
-              left: 0,
-
-              background: "pink",
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
             }}
           >
-            <audio
-              src={data.file.publicURL}
-              type="audio/mp3"
-              controls
+            <main>{children}</main>
+            <div
               style={{
-                width: "90%",
-                margin: "1em 5vw",
+                width: "100vw",
+                position: "fixed",
+                bottom: 0,
+                left: 0,
+
+                background: "pink",
               }}
-            />
+            >
+              <audio
+                src={data.file.publicURL}
+                type="audio/mp3"
+                controls
+                style={{
+                  width: "90%",
+                  margin: "1em 5vw",
+                }}
+              />
+            </div>
           </div>
-        </div>
+        ) : (
+          <main>{children}</main>
+        )}
       </>
     )}
   />
