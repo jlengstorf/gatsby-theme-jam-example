f5daf5b2df059a9c74d025188066979d41eb34c2
Reuben Ellis <reuben.ellis@spudnik.com>
2019-09-06 14:51:53 -0600

    Added proper content for demo

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -8,16 +8,16 @@ module.exports = {
   siteMetadata: {
     title: `Dashboard`,
     author: `Reuben Ellis`,
-    description: `All the Digital Tools in one place.`,
+    description: `An Authentication Site Built with Gatsby, GraphQL, Material-UI and Auth0.`,
     copyright: `This is to insert a copyright message`,
-    loginDesc: "Login",
+    loginDesc: "Login / Signup",
     isAuthApp: true,
     social: {
-      facebook: "/",
-      twitter: "/",
-      github: "ethriel3695",
-      email: "thedevelopertoolbook@gmail.com",
+      facebook: "https://www.facebook.com/altcampus",
+      twitter: "https://www.twitter.com/altcampus",
+      github: "https://www.github.com/ethriel3695",
+      email: "test@example.com",
     },
-    externalLinks: [""],
+    externalLinks: [{ label: "", link: "" }],
   },
 }
