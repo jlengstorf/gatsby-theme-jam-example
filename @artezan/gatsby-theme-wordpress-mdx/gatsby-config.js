f6b090a63de7139c0b038e705f30a464e3c3144c
Cesar Artezan <vn0193d@mx.wal-mart.com>
2019-12-17 08:28:34 -0600

    config

--- @artezan/gatsby-theme-wordpress-mdx/gatsby-config.js
+++ @artezan/gatsby-theme-wordpress-mdx/gatsby-config.js
@@ -14,19 +14,37 @@ module.exports = {
       github: ''
     }
   },
-  plugins: ['gatsby-plugin-theme-ui', 'gatsby-plugin-mdx',
-  {
-    resolve: 'gatsby-plugin-layout',
-    options: {
-      component: require.resolve('./src/layouts/layout.jsx')
+  plugins: [
+    'gatsby-plugin-theme-ui',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-layout',
+      options: {
+        component: require.resolve('./src/layouts/layout.jsx')
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: path.resolve('src/pages')
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'index',
+        path: path.resolve('src/index.mdx')
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'sections',
+        path: path.resolve('src/sections')
+      }
     }
-  }, 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'page',
-      path: path.resolve(__dirname, 'src/pages')
-    }
-  },
-]
+  ]
 }
--- @artezan/gatsby-theme-wordpress-mdx/gatsby-node.js
+++ @artezan/gatsby-theme-wordpress-mdx/gatsby-node.js
@@ -1,45 +1,35 @@
 const { createFilePath } = require('gatsby-source-filesystem')
 const path = require('path')
+const CreatePagesMdx = require(`./gatsby/create-pages-mdx`)
+
+
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === 'Mdx') {
+    const parent = getNode(node.parent)
     const value = createFilePath({ node, getNode })
     createNodeField({
       name: 'slug',
       node,
-      value: node.frontmatter.icon ? value : `/posts${value}`
+      value
     })
+    if (parent.internal.type === 'File') {
+      createNodeField({
+        name: `sourceName`,
+        node,
+        value: parent.sourceInstanceName
+      })
+    }
   }
 }
 
 exports.createPages = async ({ page, graphql, actions, reporter }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMdx {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  if (result.errors) {
-    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
-  }
-
-  result.data.allMdx.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.join(__dirname, `src/layouts/post.jsx`),
-      context: { id: node.id }
-    })
-  })
+  /**
+   * Create each page of mdx
+   */
+  await CreatePagesMdx(actions, graphql, reporter)
 }
+
+
